59e13f1d03d390e30bc8c1397daaf883f963cf8e
mthormann <mthormann@S-ANALYSIS01.BRAIN.UNI-BREMEN.DE>
2024-03-20 16:12:27 +0100

    Anpassung an neue pre_processing pipeline

--- plv_test.m
+++ plv_test.m
@@ -4,29 +4,29 @@ dir = "/data/projects/V1V4coherence/02_analysis_max/git_repos/mat_files";
 toi = [2.3 4.3];
 bp = [55 95];
 medfiltord = 20;
-load(fullfile(dir,sprintf("grand_structbp%d%dmed%dtoi%f-%f.mat",bp(1),bp(2),medfiltord,toi(1),toi(2))));
-%%
-data = grand_struct.add.out_hilbert(1);
-cfg = [];
-cfg.method = 'plv';
-cfg.channel = 'all';
-ft_connectivityanalysis(cfg, data)
+load('attout_dataset.mat')
+load('attin_dataset.mat')
+load("V4_dataset.mat")
+[in_trials,out_trials,V4_trials] = pre_processing_pip_trials(attin_dataset,attout_dataset,V4_dataset,bp,toi);
+[grand_struct,angles,inst_freq] = pre_processing_pip_hilb(in_trials,out_trials,V4_trials,medfiltord)
 
-%% Creating trials and bp filtering
+
+
+%% Creating trials and bp filtering !!!Important!!! I do not use all V4/V1 combinations yet
 in_plv = nan(16,252) %creating nan array of all sessions and max trials
 out_plv = nan(16,231) % differs from in because different max 
-for i_s = 1:length(grand_struct.add.out_hilbert)
-    for ii = 1:length(grand_struct.add.out_hilbert(i_s).trial)
-        signal_1 = grand_struct.add.out_hilbert(i_s).trial{1,ii}(1,:);
-        signal_2 = grand_struct.add.out_hilbert(i_s).trial{1,ii}(2,:);
+for i_s = 1:length(angles.out_hilbert)
+    for ii = 1:length(angles.out_hilbert(i_s).trial)
+        signal_1 = angles.out_hilbert(i_s).trial{1,ii}(1,:);
+        signal_2 = angles.out_hilbert(i_s).trial{1,ii}(2,:);
         out_phase_diff{:,ii} = signal_1-signal_2;
         out_plv(i_s,ii) = abs(mean(exp(1j * out_phase_diff{:,ii})));
     end 
 end 
-for i_s = 1:length(grand_struct.add.in_hilbert)
-    for ii = 1:length(grand_struct.add.in_hilbert(i_s).trial)
-        signal_1 = grand_struct.add.in_hilbert(i_s).trial{1,ii}(1,:);
-        signal_2 = grand_struct.add.in_hilbert(i_s).trial{1,ii}(2,:);
+for i_s = 1:length(angles.in_hilbert)
+    for ii = 1:length(angles.in_hilbert(i_s).trial)
+        signal_1 = angles.in_hilbert(i_s).trial{1,ii}(1,:);
+        signal_2 = angles.in_hilbert(i_s).trial{1,ii}(2,:);
         in_phase_diff{:,ii} = signal_1-signal_2;
         in_plv(i_s,ii) = abs(mean(exp(1j * in_phase_diff{:,ii})));
     end 
