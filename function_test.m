7196284dd50c2fcb8a401a4e517e5131729a7573
mthormann <mthormann@S-ANALYSIS01.BRAIN.UNI-BREMEN.DE>
2024-03-20 12:26:40 +0100

    Removed different median filter order functionality

--- function_test.m
+++ function_test.m
@@ -4,7 +4,7 @@ load('attout_dataset.mat')
 load('attin_dataset.mat')
 load("V4_dataset.mat")
 matpath = '/data/projects/V1V4coherence/02_analysis_max/git_repos/mat_files'
-medianorders = [20];
+medianfiltord = [20];
 bpwidth = [55 95];
 toi = [0 4.3];
 store_hilbert = true;
@@ -30,16 +30,6 @@ save(fullfile(matpath,sprintf('V4_trials%d%dtoi%f-%f.mat.mat',bpwidth(1),bpwidth
 %% Hilberting, Deriving, median filtering, inst change taking, taking mean 
 % over trials and recording sites 
 
-for med_i = 1:length(medianorders)
-clearvars -except med_i medianorders bpwidth store_hilbert toi
-matpath = '/data/projects/V1V4coherence/02_analysis_max/git_repos/mat_files'
-
-load(fullfile(matpath,sprintf('attin_trials%d%dtoi%f-%f.mat',bpwidth(1),bpwidth(2),toi(1), toi(2))));
-load(fullfile(matpath,sprintf('attout_trials%d%dtoi%f-%f.mat',bpwidth(1),bpwidth(2),toi(1), toi(2))));
-load(fullfile(matpath,sprintf('V4_trials%d%dtoi%f-%f.mat.mat',bpwidth(1),bpwidth(2),toi(1), toi(2))));
-
-
-medianfiltord = medianorders(med_i);
 parfor ii = 1:length(in_trials)
     [in_hilbertData(ii), in_diffHilbertData(ii), in_medfiltHilbert(ii),in_instchange(ii)] = do_hilbert(in_trials(ii),medianfiltord); 
 end 
@@ -111,35 +101,3 @@ grand_struct.V4summary.std = std(aMat,1,'omitnan');
 
 % saving
 save(fullfile(matpath,sprintf('grand_structbp%d%dmed%dtoi%f-%f.mat',bpwidth(1),bpwidth(2),medianfiltord, toi(1), toi(2))),'grand_struct')
-end
-
-
-%% Loading and combining different structures
-clc 
-clearvars -except med_i medianorders bpwidth
-s_in = struct();
-path = '/data/projects/V1V4coherence/02_analysis_max/git_repos/mat_files'
-for ii = 1:length(medianorders)
-    s(ii) = load(fullfile(path,sprintf('grand_structbp45100med%d',medianorders(ii))));
-    s_in(ii).input = s(ii).grand_struct.in_medfiltHilbert;
-    s_out(ii).input = s(ii).grand_struct.out_medfiltHilbert;
-    s_V4(ii).input = s(ii).grand_struct.V4_medfiltHilbert;
-end 
-[in_cell, in_sdcell] = comb_avg(s_in);
-[out_cell, out_sdcell] = comb_avg(s_out);
-[V4_cell, V4_sdcell] = comb_avg(s_V4);
-summary_struct.in_sites = in_cell;
-summary_struct.out_sites = out_cell;
-summary_struct.V4_sites = V4_cell;
-summary_struct.in_sdsites = in_sdcell;
-summary_struct.out_sdsites = out_sdcell;
-summary_struct.V4_sdsites = V4_sdcell;
-
-summary_struct.in_mean = mean(in_cell,1,'omitnan');
-summary_struct.in_sdmean = mean(in_sdcell,1,'omitnan');
-summary_struct.out_mean = mean(out_cell,1,'omitnan');
-summary_struct.out_sdmean = mean(out_sdcell,1,'omitnan');
-summary_struct.V4_mean = mean(V4_cell,1,'omitnan');
-summary_struct.V4_sdmean = mean(V4_sdcell,1,'omitnan');
-
-save(fullfile(path,sprintf('summary_structbp%d%dmed%d_%d_%d_%d_%d.mat',bpwidth(1),bpwidth(2),medianorders(1),medianorders(2),medianorders(3),medianorders(4),medianorders(5))),'summary_struct')
