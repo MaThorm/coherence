4bd330aed6e21f913f6daf66db00894b5096cb3f
mthormann <mthormann@S-ANALYSIS01.BRAIN.UNI-BREMEN.DE>
2024-03-20 16:07:17 +0100

    Two new functions emerging from previous function_test. Divided into the trial selection and the hilberting/inst_freq etc

---
+++ pre_processing_pip_hilb.m
@@ -0,0 +1,76 @@
+function [grand_struct,angles,inst_freq] = pre_processing_pip_hilb(in_trials,out_trials,V4_trials,medianfiltord)
+%PRE_PROCESSING_PIP_HILB Summary of this function goes here
+%   Detailed explanation goes here
+% Hilberting, Deriving, median filtering, inst change taking, taking mean 
+% over trials and recording sites 
+
+parfor ii = 1:length(in_trials)
+    [in_hilbertData(ii), in_diffHilbertData(ii), in_medfiltHilbert(ii),in_instchange(ii)] = do_hilbert(in_trials(ii),medianfiltord); 
+end 
+
+parfor ii = 1:length(out_trials)
+    [out_hilbertData(ii), out_diffHilbertData(ii), out_medfiltHilbert(ii),out_instchange(ii)] = do_hilbert(out_trials(ii),medianfiltord); 
+end 
+
+parfor ii = 1:length(V4_trials)
+    [V4_hilbertData(ii), V4_diffHilbertData(ii), V4_medfiltHilbert(ii), V4_instchange(ii)] = do_hilbert(V4_trials(ii),medianfiltord); 
+end 
+
+% mean over trials in 
+for ii = 1:length(in_medfiltHilbert)
+    aMat = cell2matnan(in_medfiltHilbert(ii).trial);
+    in_medfiltHilbert(ii).sess_mean = squeeze(mean(aMat,1,'omitnan'));
+    in_medfiltHilbert(ii).sess_sd = squeeze(std(aMat,1,'omitnan'));
+end 
+% mean over trials out
+for ii = 1:length(out_medfiltHilbert)
+    aMat = cell2matnan(out_medfiltHilbert(ii).trial);
+    out_medfiltHilbert(ii).sess_mean = squeeze(mean(aMat,1,'omitnan'));
+    out_medfiltHilbert(ii).sess_sd = squeeze(std(aMat,1,'omitnan'));
+end 
+
+% mean over trials V4
+for ii = 1:length(V4_medfiltHilbert)
+    aMat = cell2matnan(V4_medfiltHilbert(ii).trial);
+    V4_medfiltHilbert(ii).sess_mean = squeeze(mean(aMat,1,'omitnan'));
+    V4_medfiltHilbert(ii).sess_sd = squeeze(std(aMat,1,'omitnan'));
+end 
+
+grand_struct.out_medfiltHilbert = out_medfiltHilbert;
+grand_struct.in_medfiltHilbert = in_medfiltHilbert;
+grand_struct.V4_medfiltHilbert = V4_medfiltHilbert; 
+
+% Storing processing steps in seperate files
+angles.in_hilbert = in_hilbertData;
+angles.out_hilbert = out_hilbertData;
+angles.V4_hilbert = V4_hilbertData;
+inst_freq.in_inst = in_diffHilbertData; % named it wrong and too lazy to change
+inst_freq.out_inst = out_diffHilbertData; % should be inst_freq
+inst_freq.V4_inst = V4_diffHilbertData;
+
+
+% mean over sites in 
+site_cell = struct2cell(out_medfiltHilbert);
+site_cell = squeeze(site_cell(9,:,:));
+aMat = cell2matnan(site_cell');
+grand_struct.outsummary.mean = mean(aMat,1,'omitnan');
+grand_struct.outsummary.SEM = std(aMat,1,'omitnan')/sqrt(length(out_medfiltHilbert));
+grand_struct.outsummary.std = std(aMat,1,'omitnan');
+
+% mean over sites out 
+site_cell = struct2cell(in_medfiltHilbert);
+site_cell = squeeze(site_cell(9,:,:));
+aMat = cell2matnan(site_cell');
+grand_struct.insummary.mean = mean(aMat,1,'omitnan');
+grand_struct.insummary.SEM = std(aMat,1,'omitnan')/sqrt(length(in_medfiltHilbert));
+grand_struct.insummary.std = std(aMat,1,'omitnan');
+
+% mean over sites V4
+site_cell = struct2cell(V4_medfiltHilbert);
+site_cell = squeeze(site_cell(9,:,:));
+aMat = cell2matnan(site_cell');
+grand_struct.V4summary.mean = mean(aMat,1,'omitnan');
+grand_struct.V4summary.SEM = std(aMat,1,'omitnan')/sqrt(length(V4_medfiltHilbert));
+grand_struct.V4summary.std = std(aMat,1,'omitnan');
+end
+
---
+++ pre_processing_pip_trials.m
@@ -0,0 +1,18 @@
+function [in_trials,out_trials,V4_trials] = pre_processing_pip(attin_dataset,attout_dataset,V4_dataset,bpwidth,toi)
+%PRE_PROCESSING_PIP Summary of this function goes here
+%   Detailed explanation goes here
+
+% Creating trials and bp filtering 
+% Attin trials
+parfor ii = 1:length(attout_dataset)
+    in_trials(ii) = do_trialselection(attin_dataset(ii).path,attin_dataset(ii).file,[attin_dataset(ii).chan attin_dataset(ii).V4chan{1}],attin_dataset(ii).stimno,bpwidth,toi);
+end 
+% Attout trials
+parfor ii = 1:length(attout_dataset)
+    out_trials(ii) = do_trialselection(attout_dataset(ii).path,attout_dataset(ii).file,[attout_dataset(ii).chan attout_dataset(ii).V4chan{1}],attout_dataset(ii).stimno,bpwidth,toi);
+end 
+% AttV4 trials
+parfor ii = 1:length(V4_dataset)
+    V4_trials(ii) = do_trialselection(V4_dataset(ii).path,V4_dataset(ii).file,V4_dataset(ii).chan,V4_dataset(ii).stimno,bpwidth,toi);
+end 
+end 
