c3b0fb01d590536e631afa17845f2ba016f67f9d
mthormann <mthormann@S-ANALYSIS01.BRAIN.UNI-BREMEN.DE>
2024-03-26 19:18:45 +0100

    Added the PLC calculation the Irisw way, so far not including all recording sites

--- plv_test.m
+++ plv_test.m
@@ -11,11 +11,12 @@ load("V4_dataset.mat")
 [grand_struct,angles,inst_freq] = pre_processing_pip_hilb(in_trials,out_trials,V4_trials,medfiltord)
 
 
-%% Creating trials and bp filtering !!!Important!!! I do not use all V4/V1 combinations yet
+%% Creating trials and bp filtering
 in_plv = nan(23,252); %creating nan array of all sessions and max trials
 out_plv = nan(23,231); % differs from in because different max 
 count = 1;
 for i_sess = 1:length(angles.out_hilbert)
+    out_phase_diff = cell(1,length(angles.out_hilbert(i_sess).trial));
     for i_chan = 1:length(angles.out_hilbert(i_sess).label)-1
         for i_trial = 1:length(angles.out_hilbert(i_sess).trial)
             signal_1 = angles.out_hilbert(i_sess).trial{1,i_trial}(1,:);
@@ -28,6 +29,7 @@ for i_sess = 1:length(angles.out_hilbert)
 end 
 count = 1;
 for i_sess = 1:length(angles.in_hilbert)
+    in_phase_diff = cell(1,length(angles.in_hilbert(i_sess).trial));
     for i_chan = 1:length(angles.in_hilbert(i_sess).label)-1
         for i_trial = 1:length(angles.in_hilbert(i_sess).trial)
             signal_1 = angles.in_hilbert(i_sess).trial{1,i_trial}(1,:);
@@ -38,17 +40,41 @@ for i_sess = 1:length(angles.in_hilbert)
         count = count + 1;
     end 
 end 
-%%
 in_plv_m = mean(in_plv,2,'omitnan');
 out_plv_m = mean(out_plv,2,'omitnan');
 in_plv_fin = mean(in_plv_m);
 out_plv_fin = mean(out_plv_m);
 
 
-%%
-new = cat(2,in_plv_m,out_plv_m)
+%% 
+new = cat(2,in_plv_m,out_plv_m);
+figure;
 boxplot(new,'Labels',{'PLV attended','PLV unattended'})
 ylabel('PLV')
 title('PLV in the frequency range from 52 to 78 Hz')
+[p, value] = signrank(in_plv_m, out_plv_m)
 
-%violin(new)
+
+%% Calculating PC the Iris way
+for i_sess = 1:length(angles.in_hilbert)
+    contrib = zeros(1,2001);
+    dCos = zeros(1,2001);
+    dSin = zeros(1,2001);
+    var = angles.in_hilbert(i_sess);
+    for i_trial = 1:length(var.trial)
+        temp_array = NaN(1,2001);
+        temp_array(:,1:length(var.trial{i_trial})) = 1;
+        phase_diff = var.trial{i_trial}(1,:) - var.trial{i_trial}(2,:);
+        contrib = contrib + ~isnan(temp_array);
+        dCos(:,1:length(var.trial{i_trial})) = dCos(:,1:length(var.trial{i_trial})) + cos(phase_diff);
+        dSin(:,1:length(var.trial{i_trial})) = dSin(:,1:length(var.trial{i_trial})) + cos(phase_diff);
+    end 
+    dCos = dCos./contrib;
+    dSin = dSin./contrib;
+    VL = sqrt((dCos).^2+(dSin).^2);
+    VLEXP = sqrt(pi)./(2.*sqrt(contrib));
+    VL = VL - VLEXP;
+    meanAngle = acos((dCos)./VL);
+    meanVL(i_sess) = mean(VL);
+end 
+mean_meanVL = mean(meanVL)
