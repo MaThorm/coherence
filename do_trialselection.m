5627789e157bfb79047f67c58c99288f6e81b1f3
mthormann <mthormann@S-ANALYSIS01.BRAIN.UNI-BREMEN.DE>
2024-04-10 14:25:29 +0200

    Added parsing functionality

--- do_trialselection.m
+++ do_trialselection.m
@@ -1,4 +1,4 @@
-function [trials] = do_trialselection(path,filename,CoI,StimNo,bpwidth,toilim,BlCor,IncOut,offset)
+function [trials] = do_trialselection(path,filename,CoI,StimNo,bpfilt,varargin)
 %DO_TRIALSELECTION 
 %   path = path of the file
 %   filename = name of the file
@@ -10,9 +10,38 @@ function [trials] = do_trialselection(path,filename,CoI,StimNo,bpwidth,toilim,Bl
 %   IncOut = Included Outcomes (default = [0])
 %   offset = value in s by which x-axis gets reoriented (default = 1.3)
 
-if ~exist('BlCor'), BlCor = false;end
-if ~exist('IncOut'), IncOut = [0];end
-if ~exist('offset'), offset = 1.3;end
+% Input parsing
+
+p = inputParser;
+
+dbpwidth = [1 150];
+dtoilim = [-100 100];
+dblcor = false;
+dincout = [0];
+doffset = 1.3;
+
+addRequired(p,'path', @(x) ischar(x));
+addRequired(p,'filename', @(x) ischar(x));
+addRequired(p,'CoI',@(x) isnumeric(x));
+addRequired(p,'StimNo',@(x) isnumeric(x));
+addRequired(p,'bpfilt',@(x) islogical(x));
+addParameter(p,'bpwidth',dbpwidth,@(x) isnumeric(x));
+addParameter(p,'toilim',dtoilim,@(x) isnumeric(x));
+addParameter(p,'blcor',dblcor,@(x) islogical(x)); 
+addParameter(p,'IncOut',dincout,@(x) isnumeric(x));
+addParameter(p,'offset',doffset, @(x) isnumeric(x));
+parse(p,path,filename,CoI,StimNo,bpfilt,varargin{:});
+
+path = p.Results.path;
+filename = p.Results.filename;
+CoI = p.Results.CoI;
+StimNo = p.Results.StimNo;
+bpfilt = p.Results.bpfilt;
+toilim = p.Results.toilim;
+blcor = p.Results.blcor;
+IncOut = p.Results.IncOut;
+offset = p.Results.offset;
+ 
 
 filename = fullfile(path,filename);
 vblTrigger = 15;
@@ -45,33 +74,29 @@ selectCfg.trials = ismember(trials.trialinfo(:,3), IncOut) & ...
 trials = ft_selectdata(selectCfg, trials);
 
 % selecting certain time window
-if exist('toilim') 
-    cfg = [];
-    cfg.toilim = toilim;
-    trials = ft_redefinetrial(cfg, trials);
-end 
+cfg = [];
+cfg.toilim = toilim;
+trials = ft_redefinetrial(cfg, trials);
 
 % shift 0 by offset
 cfg = [];
 cfg.offset = -round(offset*data.hdr.Fs);
 trials = ft_redefinetrial(cfg, trials);
 
-
-%{  
-remove average
-if BlCor == true
+if blcor == true
     preprocCfg = [];
     preprocCfg.demean = 'yes';
     preprocCfg.baselinewindow = [-1.3 -0.5]
     trials = ft_preprocessing(preprocCfg, trials);
 end 
-%}
-%
+
 % BP filtering
-cfg = [];
-cfg.bpfilter = 'yes';
-cfg.bpfreq = bpwidth;
-cfg.bpfilttype = 'fir'; % Could different fooof results be due to this?
-trials = ft_preprocessing(cfg,trials);
+if bpfilt == true
+    cfg = [];
+    cfg.bpfilter = 'yes';
+    cfg.bpfreq = p.Results.bpwidth;
+    cfg.bpfilttype = 'fir'; 
+    trials = ft_preprocessing(cfg,trials);
+end 
 %
 end 
