1dbcf3b28357112a0e90840cc994ba80f980b33e
mthormann <mthormann@S-ANALYSIS01.BRAIN.UNI-BREMEN.DE>
2024-07-23 13:26:19 +0200

    Testing Scripts

--- testing_V1V4cor.m
+++ testing_V1V4cor.m
@@ -1,3 +1,4 @@
+%% Script to test and plot the V1/V4 correlations 
 clc
 clear 
 % Parameter Settings
@@ -8,9 +9,7 @@ load("/data/projects/V1V4coherence/02_analysis_max/git_repos/params.mat")
 
 
 % Loading for  analysis elongating
-% filename = sprintf("inst_freq_cut_toi%.1f-%.1f_sdmult%.1f_lower%d_upper%d.mat",params.toi(1),params.toi(2),params.sd_mult,params.lower,params.upper);
-filename = sprintf("inst_freq_toi%.1f-%.1f_lower%i_upper%i_filtord%i.mat",params.toi(1),params.toi(2),params.lower,params.upper,params.filtord);
-% m = matfile(fullfile(params.matpath,'Inst_freq_cut',filename),'Writable',true);
+filename = sprintf("inst_freq_%s_toi%.1f-%.1f_lower%i_upper%i_filttype%s_filtord%i_framelen%i.mat",params.bptype,params.toi(1),params.toi(2),params.lower,params.upper,params.filttype,params.filtord,params.framelen);
 m = matfile(fullfile(params.matpath,'Inst_freq',filename),'Writable',true);
 inst_freq = m.inst;
 
@@ -22,12 +21,7 @@ inst_freq.V4 = elongate_V4(inst_freq.V4)
 
 [inst_mean.in, grand_inst_mean.in] = do_grand_avg(inst_freq.in,true)
 [inst_mean.out, grand_inst_mean.out] = do_grand_avg(inst_freq.out,true)
-inst_mean.V4 = do_grand_avg(inst_freq.V4)
-% %% Correlation based on sessions with full V4
-% for ii = 1:length(inst_freq.in)
-%     [in_corr.sess(1,ii) in_corr.sess(2,ii)] = corr(inst_mean.in(ii).avg(1,:)',inst_mean.V4(ii).avg');
-%     [out_corr.sess(1,ii) out_corr.sess(2,ii)]  = corr(inst_mean.out(ii).avg(1,:)',inst_mean.V4(ii).avg');
-% end 
+[inst_mean.V4, grand_inst_mean.V4] = do_grand_avg(inst_freq.V4)
 
 
 % Correlation based on sessions with seperated V4
@@ -35,26 +29,26 @@ for ii = 1:length(inst_freq.in)
     [in_corr.sess(1,ii) in_corr.sess(2,ii)] = corr(inst_mean.in(ii).avg(1,:)',inst_mean.in(ii).avg(2,:)');
     [out_corr.sess(1,ii) out_corr.sess(2,ii)]  = corr(inst_mean.out(ii).avg(1,:)',inst_mean.out(ii).avg(2,:)');
 end 
-
-% Calculating trial based correlations
-for i_s = 1:length(inst_freq.in)
-    cur_in = inst_freq.in(i_s)
-    cur_out = inst_freq.out(i_s)
-    for i_t = 1:length(cur_in.trial)
-        [in_corr.trial(i_s).r(i_t) in_corr.trial(i_s).p(i_t)] = corr(cur_in.trial{1,i_t}(1,:)',cur_in.trial{1,i_t}(2,:)');
-    end 
-    in_corr.trial(i_s).r_mean = mean(in_corr.trial(i_s).r);
-    in_corr.trial(i_s).rp_mean = mean(in_corr.trial(i_s).r(in_corr.trial(i_s).p > 0.05));
-
-    for i_t = 1:length(cur_out.trial)
-        [out_corr.trial(i_s).r(i_t) out_corr.trial(i_s).p(i_t)] = corr(cur_out.trial{1,i_t}(1,:)',cur_out.trial{1,i_t}(2,:)');
-    end 
-    out_corr.trial(i_s).r_mean = mean(out_corr.trial(i_s).r);
-end 
 out_corr.sess_m = mean(out_corr.sess,2)
 in_corr.sess_m = mean(in_corr.sess,2)
+% % Calculating trial based correlations
+% for i_s = 1:length(inst_freq.in)
+%     cur_in = inst_freq.in(i_s)
+%     cur_out = inst_freq.out(i_s)
+%     for i_t = 1:length(cur_in.trial)
+%         [in_corr.trial(i_s).r(i_t) in_corr.trial(i_s).p(i_t)] = corr(cur_in.trial{1,i_t}(1,:)',cur_in.trial{1,i_t}(2,:)');
+%     end 
+%     in_corr.trial(i_s).r_mean = mean(in_corr.trial(i_s).r);
+%     in_corr.trial(i_s).rp_mean = mean(in_corr.trial(i_s).r(in_corr.trial(i_s).p > 0.05));
+% 
+%     for i_t = 1:length(cur_out.trial)
+%         [out_corr.trial(i_s).r(i_t) out_corr.trial(i_s).p(i_t)] = corr(cur_out.trial{1,i_t}(1,:)',cur_out.trial{1,i_t}(2,:)');
+%     end 
+%     out_corr.trial(i_s).r_mean = mean(out_corr.trial(i_s).r);
+% end 
+
 %% Plotting V1a V1n, and V4 + correlation 
-foldername = fullfile(params.figpath,'inst_freq_23',sprintf('%d-%d/toi%.1f-%.1f/correlations',params.lower,params.upper,params.toi(1),params.toi(2)))
+foldername = fullfile(params.figpath,'inst_freq',params.bptype,sprintf("bpwidth_%i-%i/toi_%.1f-%.1f/V1V4corr/sess_corr_plots",params.lower,params.upper,params.toi(1),params.toi(2)))
 f = figure;
 f.Units = 'normalized';
 f.Position = [0.25 0.25 0.5 0.5]
@@ -83,9 +77,9 @@ for ii = 1:length(in_corr.sess)
     if ~exist(foldername,'dir')
         mkdir(foldername)
     end     
-%     saveas(f,fullfile(foldername,sprintf('In_out_V4_corr_sess%i.fig',ii)))
-%     saveas(f,fullfile(foldername,sprintf('In_out_V4_corr_sess%i.png',ii)))
-    w = waitforbuttonpress;
+    saveas(f,fullfile(foldername,sprintf('In_out_V4_corr_sess%i.fig',ii)))
+    saveas(f,fullfile(foldername,sprintf('In_out_V4_corr_sess%i.png',ii)))
+%     w = waitforbuttonpress;
     clf
 end 
 
@@ -96,7 +90,7 @@ y = x;
 f = figure; 
 f.Units = 'normalized'
 f.Position = [0.25 0.25 0.5 0.5]
-foldername = fullfile(params.figpath,'inst_freq_23',sprintf('%d-%d/toi%.1f-%.1f/scatter_cor',params.lower,params.upper,params.toi(1),params.toi(2)))
+foldername = fullfile(params.figpath,'inst_freq',params.bptype,sprintf("bpwidth_%i-%i/toi_%.1f-%.1f/V1V4corr",params.lower,params.upper,params.toi(1),params.toi(2)))
 if ~exist(foldername,'dir')
     mkdir(foldername)
 end 
@@ -106,13 +100,13 @@ hold on
 plot(x,y)
 xl = xline([0],'--','color',[0.7 0.7 0.7]);
 yl = yline([0],'--','color',[0.7 0.7 0.7]);
-annotation(f,'textbox',[0.1 0.01 0.8 0.1],'String',sprintf("toi: %.1f - %.1f, filtord: %i,\nAttend In cor: %.2f, Attend Out cor: %.2f", ...
-    params.toi(1),params.toi(2),params.filtord,in_corr.sess_m(1,1),out_corr.sess_m(1,1)))
+annotation(f,'textbox',[0.1 0.01 0.8 0.1],'String',sprintf("MC: %s , filttype: %s, \nAttend In cor: %.2f, Attend Out cor: %.2f", ...
+    num2str(params.MC),params.bptype,in_corr.sess_m(1,1),out_corr.sess_m(1,1)))
 title('In vs Out V1/V4 correlations')
 ylabel('V1/V4 AttendIn')
 xlabel('V1/V4 AttendOut')
-saveas(gcf,fullfile(foldername,sprintf("In_vs_out_Scatter_filtord%i.fig",params.filtord)))
-saveas(gcf,fullfile(foldername,sprintf("In_vs_out_Scatter_filtord%i.jpg",params.filtord)))
+saveas(gcf,fullfile(foldername,sprintf("In_vs_out_Scatter.fig",params.filtord)))
+saveas(gcf,fullfile(foldername,sprintf("In_vs_out_Scatter.jpg",params.filtord)))
 
 
 
@@ -137,7 +131,7 @@ in_corr_det.sess_m = mean(in_corr_det.sess,2)
 out_corr_det.sess_m = mean(out_corr_det.sess,2)
 
 %% Plotting V1a V1n, and V4 + correlation detrended
-foldername = fullfile(params.figpath,'inst_freq_23',sprintf('%d-%d/toi%.1f-%.1f/correlations_detrend_filtord%i',params.lower,params.upper,params.toi(1),params.toi(2),params.filtord))
+foldername = fullfile(params.figpath,'inst_freq',params.bptype,sprintf("bpwidth_%i-%i/toi_%.1f-%.1f/V1V4corr/detrended/sess_corr_plots",params.lower,params.upper,params.toi(1),params.toi(2)))
 f = figure;
 f.Units = 'normalized';
 f.Position = [0 0 1 1]
@@ -155,7 +149,7 @@ for ii = 1:length(in_corr.sess)
 
     subplot(1,2,2)
     plot(inst_mean_det.out(ii).avg(1,:),'b')
-    hold on mean
+    hold on
     plot(inst_mean_det.out(ii).avg(2,:),'Color',"#EDB120")
     title(sprintf("V1n & V4 inst. freq, pair %i. r = %.2f, p = %.2f",ii,out_corr.sess(1,ii),out_corr.sess(2,ii)))
     legend({'AttendOut','V4'})
@@ -168,15 +162,15 @@ for ii = 1:length(in_corr.sess)
     saveas(f,fullfile(foldername,sprintf('In_out_V4_corr_sess%i.fig',ii)))
     saveas(f,fullfile(foldername,sprintf('In_out_V4_corr_sess%i.png',ii)))
 %     w = waitforbuttonpress;
-    clf
+%     clf
 end 
-% Scatterplot of attend in vs attend out correlations with V4
+%% Scatterplot of attend in vs attend out correlations with V4
 x = -0.5:0.01:1
 y = x;
 f = figure; 
 f.Units = 'normalized'
 f.Position = [0.25 0.25 0.5 0.5]
-foldername = fullfile(params.figpath,'inst_freq_23',sprintf('%d-%d/toi%.1f-%.1f/scatter_cor_detrend',params.lower,params.upper,params.toi(1),params.toi(2)))
+foldername = fullfile(params.figpath,'inst_freq',params.bptype,sprintf("bpwidth_%i-%i/toi_%.1f-%.1f/V1V4corr/detrended",params.lower,params.upper,params.toi(1),params.toi(2)))
 if ~exist(foldername,'dir')
     mkdir(foldername)
 end 
@@ -186,8 +180,8 @@ hold on
 plot(x,y)
 xl = xline([0],'--','color',[0.7 0.7 0.7]);
 yl = yline([0],'--','color',[0.7 0.7 0.7]);
-annotation(f,'textbox',[0.1 0.01 0.8 0.1],'String',sprintf("toi: %.1f - %.1f, filtord: %i,\nAttend In cor: %.2f, Attend Out cor: %.2f", ...
-    params.toi(1),params.toi(2),params.filtord,in_corr_det.sess_m(1,1),out_corr_det.sess_m(1,1)))
+annotation(f,'textbox',[0.1 0.01 0.8 0.1],'String',sprintf("MC: %s , filttype: %s, \nAttend In cor: %.2f, Attend Out cor: %.2f", ...
+    num2str(params.MC),params.bptype,in_corr_det.sess_m(1,1),out_corr_det.sess_m(1,1)))
 title('In vs Out V1/V4 correlations')
 ylabel('V1/V4 AttendIn')
 xlabel('V1/V4 AttendOut')
@@ -195,7 +189,7 @@ saveas(gcf,fullfile(foldername,sprintf("In_vs_out_Scatter_filtord%i.fig",params.
 saveas(gcf,fullfile(foldername,sprintf("In_vs_out_Scatter_filtord%i.jpg",params.filtord)))
 
 %% Saving the correlation structures
-filename = fullfile(params.matpath,"V1_V4correlations",sprintf("V1_V4correlations_toi%.1f-%.1f_filtord%i_bounds%i-%i.mat",params.toi(1),params.toi(2),params.filtord,params.lower,params.upper))
+filename = fullfile(params.matpath,"V1_V4correlations",sprintf("V1_V4correlations_%s_toi%.1f-%.1f_bounds%i-%i.mat",params.bptype,params.toi(1),params.toi(2),params.lower,params.upper))
 save(filename,"in_corr","in_corr_det","out_corr","out_corr_det")
 %% Functions
 function [new_avg] =  my_detrend(cur_avg)
