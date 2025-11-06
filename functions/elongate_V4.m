d82078efd8fdcbea669c5afe80cc1f7bc0266ff7
MaThorm <mthorm96@gmail.com>
2025-11-06 16:17:26 +0100

    Delete functions/elongate_V4.m

--- functions/elongate_V4.m
+++
@@ -1,8 +0,0 @@
-function [cur_V4] = elongate_V4(V4_struct)
-%ELONGATES and Orders the V4 Struct
-v4_long = [1 1 2 3 2 3 4 5 6 7 8 9 9 10 11 12 13 14 15 13 14 15 16]
-for ii = 1:length(v4_long)
-    cur_V4(ii) = V4_struct(v4_long(ii))
-end 
-end
-
